4135e508c39d9b60440df2b7c51ad5c0867b280f
leczgonzaga98 <leczgonzaga98@gmail.com>
2025-12-19 12:01:09 +0800

    move readme

--- Sample/obj/Debug/net8.0/Sample.AssemblyInfo.cs
+++ Sample/obj/Debug/net8.0/Sample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d88725d93312d378dfaffefac7ebad35196f126e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
